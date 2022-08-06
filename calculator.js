bc1619d950abaad02833aab543b739187e324749
Md-Mubarak-Hossain <hossainmubarak477@gmail.com>
2022-08-06 13:55:39 +0600

    calculator added

--- calculator.js
+++ calculator.js
@@ -1,6 +1,32 @@
-let numbersBox = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-function myCalculator(number) {
-    let x = document.forms['myform']['inputValue'].value;
 
+
+
+// Use insert() function to insert the number in textview.  
+
+
+function insert(number) {
+    document.form1.textview.value = document.form1.textview.value + number;
 }
-myCalculator(numbersBox);
+
+
+
+// Use equal() function to return the result based on passed values. 
+
+
+function equal() {
+    let x = document.form1.textview.value;
+    if (x) {
+        document.form1.textview.value = eval(x)
+    }
+}
+
+/* Here, we create a backspace() function 
+to remove the number at the end of 
+the numeric series in textview. */
+
+
+function backspace() {
+    let x = document.form1.textview.value;
+    document.form1.textview.value
+        = x.substring(0, x.length - 1); /* remove the element from total length ? 1 */
+}
